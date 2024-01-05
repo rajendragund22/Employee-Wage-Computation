e7a67e7390314dd1f885471c010cf05b4ec54146
Rajendra Gund <rajendragund22@gmail.com>
2024-01-05 16:47:48 +0530

    [Rajendra] Updated : Updated the code for check emp is present or not

--- src/com/empwage/EmpWageComp.java
+++ src/com/empwage/EmpWageComp.java
@@ -4,5 +4,12 @@ public class EmpWageComp {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
+		int IS_PRESENT = 1;
+		int empCheck = (int) Math.floor(Math.random() * 2);
+		if (empCheck == IS_PRESENT) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
 	}
 }
