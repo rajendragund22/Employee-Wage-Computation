45670495d63d84c5310df1c1a4cae52ac6c833c9
Rajendra Gund <rajendragund22@gmail.com>
2024-01-05 17:03:37 +0530

    [Rajendra] Updated : Updated the code for calculate daily emp wage

--- src/com/empwage/EmpWageComp.java
+++ src/com/empwage/EmpWageComp.java
@@ -5,11 +5,18 @@ public class EmpWageComp {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
 		int IS_PRESENT = 1;
+		int WAGE_PER_HR = 20;
+		int hrOfDay;
 		int empCheck = (int) Math.floor(Math.random() * 2);
 		if (empCheck == IS_PRESENT) {
 			System.out.println("Employee is Present");
+			hrOfDay=8;
+			
 		} else {
 			System.out.println("Employee is Absent");
+			hrOfDay=0;
 		}
+		int empWage=WAGE_PER_HR*hrOfDay ;
+		System.out.println("Daily Employee wage is : "+ empWage);
 	}
 }
