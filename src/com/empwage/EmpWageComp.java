fb10242ef51d51d420d6be8cc75feb111274ebc1
Rajendra Gund <rajendragund22@gmail.com>
2024-01-10 11:52:56 +0530

    [Rajendra] Update : Updated the code with calculating the wage for a month

--- src/com/empwage/EmpWageComp.java
+++ src/com/empwage/EmpWageComp.java
@@ -4,41 +4,31 @@ public class EmpWageComp {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
-		int IS_PRESENT = 1;
-		int IS_PART_TIME = 2;
 		int WAGE_PER_HR = 20;
+		int DAY_PER_MONTH = 20;
 		int hrOfDay;
-		int empCheck = (int) Math.floor(Math.random() * 3);
-		switch (empCheck) {
-		case 1: {
-			System.out.println("Employee is Present");
-			hrOfDay = 8;
-			break;
-		}
-		case 2: {
-			System.out.println("Employee is Part time");
-			hrOfDay = 4;
-			break;
-		}
+		int totalWage = 0;
+		for (int i = 1; i < DAY_PER_MONTH + 1; i++) {
+			int empCheck = (int) Math.floor(Math.random() * 3);
+			switch (empCheck) {
+			case 1:
+				System.out.println("Employee is Present");
+				hrOfDay = 8;
+				break;
 
-		default: {
-			System.out.println("Employee is Absent");
-			hrOfDay = 0;
+			case 2:
+				System.out.println("Employee is Part time");
+				hrOfDay = 4;
+				break;
+
+			default:
+				System.out.println("Employee is Absent");
+				hrOfDay = 0;
+			}
+			int empWage = WAGE_PER_HR * hrOfDay;
+			System.out.println("Day " + i + " Employee wage is : " + empWage);
+			totalWage = totalWage + empWage;
 		}
-		}
-//		if (empCheck == IS_PRESENT) {
-//			System.out.println("Employee is Present");
-//			hrOfDay = 8;
-//
-//		} else if (empCheck == IS_PART_TIME) {
-//			System.out.println("Employee is Part time");
-//			hrOfDay = 4;
-//
-//		} else {
-//			System.out.println("Employee is Absent");
-//			hrOfDay = 0;
-//		}
-		int empWage = WAGE_PER_HR * hrOfDay;
-		System.out.println("Daily Employee wage is : " + empWage);
+		System.out.println("Total Emp wage for 20 days = " +totalWage);
 	}
 }
