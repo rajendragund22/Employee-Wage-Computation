db31d3c5e764f917c2db76ca1b9729d6d88aaa5b
Rajendra Gund <rajendragund22@gmail.com>
2024-01-10 17:56:16 +0530

    [Rajendra] Update : Updated the code with refactored code with the class and method

--- src/com/empwage/EmpWageComp.java
+++ src/com/empwage/EmpWageComp.java
@@ -1,19 +1,23 @@
 package com.empwage;
 
 public class EmpWageComp {
+	final static int WAGE_PER_HR = 20;
+	final static int DAY_PER_MONTH = 20;
+	final static int MAX_WORKING_DAYS = 20;
+	final static int MAX_WORKING_HRS = 100;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
-		int WAGE_PER_HR = 20;
-		int DAY_PER_MONTH = 20;
-		int MAX_WORKING_DAYS = 20;
-		int MAX_WORKING_HRS = 100;
+		EmployeeWageComp();
+	}
+
+	public static void EmployeeWageComp() {
 		int hrOfDay;
 		int totalWage = 0;
 		int totalWorkingHrs = 0;
 		int totalWorkingDays = 0;
 		while (totalWorkingHrs <= MAX_WORKING_HRS && totalWorkingDays < MAX_WORKING_DAYS) {
-            totalWorkingDays++;
+			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 3);
 			switch (empCheck) {
 			case 1:
@@ -30,14 +34,13 @@ public class EmpWageComp {
 				System.out.println("Employee is Absent");
 				hrOfDay = 0;
 			}
-			 totalWorkingHrs += hrOfDay;
+			totalWorkingHrs += hrOfDay;
 			int empWage = WAGE_PER_HR * hrOfDay;
 			System.out.println("Day  Employee wage is : " + empWage);
 			totalWage = totalWage + empWage;
 		}
-	     System.out.println("Total working hrs = " + totalWorkingHrs);
+		System.out.println("Total working hrs = " + totalWorkingHrs);
 		System.out.println("Total Emp wage for 20 days = " + totalWage);
-
 	}
 
 }
